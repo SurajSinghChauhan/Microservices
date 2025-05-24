a02cc4554add4cbac387db8f4267229eaa92dece
Suraj Kumar Singh <surajkrsingh825@gmail.com>
2025-05-24 12:41:48 +0530

    Microservices

---
+++ CommandsService/Program.cs
@@ -0,0 +1,38 @@
+using CommandsService.Data;
+using CommandsService.EventProcessing;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMen"));
+builder.Services.AddScoped<ICommandRepo, CommandRepo>();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+
+builder.Services.AddSingleton<IEventProcessor, EventProcessor>();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+//builder.Services.AddScoped<IPlatformDataClient, PlatformDataClient>();
+
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
